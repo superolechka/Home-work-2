a77edc8114803c187412c5df3f4a4f3718e31e60
Skorobagach Olga <skorobagach.math@gmail.com>
2023-02-28 18:47:21 +0200

    change hw2.js

--- js/hw2.js
+++ js/hw2.js
@@ -1,10 +1,22 @@
 const operand1 = Number(prompt("Введіть перше число"));
 const operand2 = Number(prompt("Введіть друге число"));
-const add = operand1 + operand2;
-console.log('Сума: ' + operand1 + '+' + operand2 + ' = ' + add );
-const sub = operand1 - operand2;
-console.log('Різниця: ' + operand1 + '-' + operand2 + ' = ' + sub);
-const mult = operand1 * operand2;
-console.log('Добуток: ' + operand1 + '*' + operand2 + ' = ' + mult);
-const div = operand1 / operand2;
-console.log('Частка: ' + operand1 + '/' + operand2 + ' = ' + div);  
+
+function sum(operand1, operand2) {
+    return operand1 + operand2
+}
+console.log('Сума: ' + operand1 + '+' + operand2 + ' = ' + sum(operand1, operand2) );
+
+function sub(operand1, operand2) {
+    return operand1 - operand2
+}
+console.log('Різниця: ' + operand1 + '-' + operand2 + ' = ' + sub(operand1, operand2));
+
+function mult(operand1, operand2) {
+    return operand1 * operand2
+}
+console.log('Добуток: ' + operand1 + '*' + operand2 + ' = ' + mult(operand1, operand2));
+
+function div(operand1, operand2) {
+    return operand1 / operand2
+}
+console.log('Частка: ' + operand1 + '/' + operand2 + ' = ' + div(operand1, operand2));  
